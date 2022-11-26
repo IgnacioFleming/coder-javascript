5e87859586da0f0987db1bc4bd471af4b31f6090
Ignacio Fleming <ignacioflemings@gmail.com>
2022-11-26 12:16:11 -0300

    armamos el simulador de la billetera con ingreso, egreso, consulta y presupuesto

--- main.js
+++ main.js
@@ -6,14 +6,14 @@
 
 alert("Antes de empezar por favor siga los pasos para crear una cuenta");
 
-const userAuth=prompt("Elija un nombre de Usuario para su cuenta");
-const passwordAuth=prompt("Elija una contraseña para su cuenta");
+const usuarioAutorizado=prompt("Elija un nombre de Usuario para su cuenta");
+const contraAutorizada=prompt("Elija una contraseña para su cuenta");
 let intento = 2
-for(i=1 ; i<=3 ; i++){
-    let user = prompt("Ingrese su nombre de Usuario.");
-    let password = prompt("Ingrese su contraseña");
+for(let i=1 ; i<=3 ; i++){
+    let usuario = prompt("Ingrese su nombre de Usuario.");
+    let contraseña = prompt("Ingrese su contraseña");
     console.log(i);
-    if(user == userAuth && password == passwordAuth){
+    if(usuario == usuarioAutorizado && contraseña == contraAutorizada){
         alert("Bienvenido! Comience a usar Easy Budget");
         break;
     } else {
@@ -31,3 +31,94 @@ for(i=1 ; i<=3 ; i++){
         
     }
 }
+
+
+/*Una vez iniciada la sesion, ingresamos al menú principal.
+Tenemos la opcion de declarar Ingresos, Egresos y armar presupuestos.*/
+let saldo = 0;
+let valor;
+let opcion;
+let opcionPresupuesto;
+let comida = 0;
+let alquiler = 0;
+let transporte = 0;
+let ahorro = 0;
+let Suma = (a,b) => a + b;
+let Resta = (a,b) => a - b;
+do{
+    opcion = prompt("Ingrese 'I' para declarar un Ingreso, 'E' para declarar un Egreso, 'P' para reservar dinero para un gasto presupuestado, 'C' para consultar sus Saldos y 'S' para salir");
+
+    switch (opcion){
+        case "I":
+            saldo = Suma(saldo,input(valor))
+            muestraSaldo()
+            break;
+        case "E":
+            saldo = Resta(saldo,input(valor))
+            muestraSaldo()
+            break;
+        case "P":
+            do{
+                muestraSaldo()
+                opcionPresupuesto = prompt("Para asignar dinero a su presupuesto ingrese 'comida', 'alquiler', 'transporte' o 'ahorro', según corresponda. En caso contrario ingrese 'salir' para volver al menú anterior")
+                switch (opcionPresupuesto){
+                    case "comida":
+                        comida = Suma(comida,input(valor))
+                        saldo = Resta(saldo,comida)
+                        break;
+                    
+                    case "alquiler":
+                        alquiler = Suma(alquiler,input(valor))
+                        saldo = Resta(saldo,alquiler)
+                        break;
+                    case "transporte":
+                        transporte = Suma(transporte,input(valor))
+                        saldo = Resta(saldo,transporte)
+                        break;
+                    case "ahorro":
+                        ahorro = Suma(ahorro,input(valor))
+                        saldo = Resta(saldo,ahorro)
+                        break;
+
+                    case "salir":
+                        break;
+                    
+                    default:
+                        alert("La opción ingresada no es válida, vuelva a intentarlo")
+                }
+            }while(opcionPresupuesto != "salir")
+
+            break;
+        case "C":
+            consultaSaldos()
+            break;
+
+        case "S":
+            alert("Su sesión ha sido finalizada")
+            break;
+        default:
+            alert("Ingresó una opción no válida, vuelva a intentarlo")
+
+        
+
+    }
+} while(opcion!="S")
+
+function input(valor){
+    valor = parseInt(prompt("Ingrese el monto a imputar"));
+    return valor;
+}
+function muestraSaldo(){
+    console.log("Su saldo disponible es de $" + saldo)
+}
+
+function consultaSaldos(){
+    console.log("Su saldo disponible es de $",saldo)
+    console.log("Su saldo asignado a comida es de $",comida)
+    console.log("Su saldo asignado a alquileres es de $",alquiler)
+    console.log("Su saldo asignado a transporte es de $",transporte)
+    console.log("Su saldo asignado a ahorro es de $",ahorro)
+}
+
+
+
