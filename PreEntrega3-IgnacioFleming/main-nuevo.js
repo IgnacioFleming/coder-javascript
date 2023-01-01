ade724a8627c13bd43d9d9f88a7cb910e681dde0
Ignacio Fleming <ignacioflemings@gmail.com>
2023-01-01 13:35:42 -0300

    fixeo problemas en modificar cliente

--- PreEntrega3-IgnacioFleming/main-nuevo.js
+++ PreEntrega3-IgnacioFleming/main-nuevo.js
@@ -45,8 +45,9 @@ const alta = document.getElementById("alta");
 const abrirFormAlta = () =>{
     display.innerHTML= "";
     const form = document.createElement("div");
-    form.classList.add("col-6", "col-md-6", "col-xl-6");
-    form.innerHTML =    `<form id="formulario">
+    form.classList.add("col-12", "col-md-6", "col-xl-6");
+    form.innerHTML =    `<h2 class="mt-5">Ingrese los datos del nuevo cliente:</h2>
+                            <form id="formulario">
                             <div class="mb-3 mt-5">
                                 <label for="nombre" class="form-label">Nombre:</label>
                                 <input type="text" class="form-control" id="nombre">
@@ -69,7 +70,9 @@ const abrirFormAlta = () =>{
                         </form>
                      `
     display.appendChild(form);
+    //Vinculamos el formulario creado en JS.
     const formulario = document.getElementById("formulario");
+    //Le damos vida al boton de Dar de Alta para que cree el objeto Nuevo Cliente y lo pushee al array.
     formulario.addEventListener("submit", (e) =>{
     e.preventDefault();
     const nombre = document.getElementById("nombre");
@@ -80,7 +83,6 @@ const abrirFormAlta = () =>{
     //creamos el objeto nuevo cliente
     const nuevoCliente = new Cliente(nombre.value, apellido.value, dni.value, mail.value);
     clientes.push(nuevoCliente);
-    console.log(clientes);
     nombre.value = "";
     apellido.value = "";
     dni.value = "";
@@ -91,6 +93,84 @@ const abrirFormAlta = () =>{
 //Creamos evento al hacer click en boton Nuevo Cliente para cargar el formulario.
 alta.addEventListener("click", () => {abrirFormAlta()});
 
-//Vinculamos el formulario creado en html
+//2)Modificar Datos del cliente.
+//Primero vinculamos el nodo.
+const btnModificar = document.getElementById("btnModificar");
+btnModificar.addEventListener("click", () => {
+    display.innerHTML= "";
+    const buscaCliente = document.createElement("div");
+    buscaCliente.classList.add("col-12", "col-md-6", "col-xl-6")
+    buscaCliente.innerHTML=`<h2 class="mt-5 mb-3">Ingrese el DNI del cliente a Modificar</h2>
+                            <form id="buscar">
+                                <input id="dniModificar" class="form-control mb-3 col-3 col-md-3 col-xl-3" type="text"></input>
+                                <button class="btn btn-primary mb-3">Buscar</button>
+                            </form>
+    `
+    display.appendChild(buscaCliente);
+    const buscar = document.getElementById("buscar");
+    buscar.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const dniModificar = document.getElementById("dniModificar");
+        const clienteModificacion = clientes.find(cliente => cliente.dni === dniModificar.value);
+        console.log(clienteModificacion);
+        const indiceCliente = clientes.indexOf(clienteModificacion);
+        const modificarCliente = () => {
+            display.innerHTML= "";
+            const formModificacion = document.createElement("div");
+            formModificacion.classList.add("col-12", "col-md-6", "col-xl-6");
+            formModificacion.innerHTML =    `<h2 class="mt-5">Ingrese los nuevos datos del cliente a modificar:</h2>
+                                            <form id="formularioModificacion">
+                                                <div class="mb-3 mt-5">
+                                                    <label for="nombreModificacion" class="form-label">Nombre:</label>
+                                                    <input type="text" class="form-control" id="nombreModificacion">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="apellidoModificacion" class="form-label">Apellido:</label>
+                                                    <input type="text" id="apellidoModificacion" class="form-control" rows="3"></input>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="dniModificacion" class="form-label">DNI:</label>
+                                                    <input type="text" class="form-control" id="dniModificacion" rows="3"></input>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="mailModificacion"  class="form-label">Email:</label>
+                                                    <input type="email" class="form-control" id="mailModificacion" rows="3" placeholder="nombre@ejemplo.com"></input>
+                                                </div>
+                                                <div class="mb-5">
+                                                    <button id="modificarCliente" class="btn btn-primary">Modificar</button>
+                                                </div>
+                                            </form>
+                                             `
+            display.appendChild(formModificacion);
+            //Vinculamos el formulario creado en JS.
+            const formularioModificacion = document.getElementById("formularioModificacion");
+            //Le damos vida al boton de Dar de Alta para que cree el objeto Nuevo Cliente y lo pushee al array.
+            formularioModificacion.addEventListener("submit", (e) =>{
+            e.preventDefault();
+            const nombreModificacion = document.getElementById("nombreModificacion");
+            const apellidoModificacion = document.getElementById("apellidoModificacion");
+            const dniModificacion = document.getElementById("dniModificacion");
+            const mailModificacion = document.getElementById("mailModificacion");
+            //creamos el objeto nuevo cliente
+            const clienteModificado = new Cliente(nombreModificacion.value, apellidoModificacion.value, dniModificacion.value, mailModificacion.value);
+            clientes.splice(clienteModificacion, 1, clienteModificado);
+            nombreModificacion.value = "";
+            apellidoModificacion.value = "";
+            dniModificacion.value = "";
+            mailModificacion.value = "";
+            })
+        
+        }
+        if (indiceCliente == -1){
+            display.innerHTML = `<h2>El DNI ingresado no corresponde a un cliente de la base</h2>`
+        }else{
+            modificarCliente();
+            
+        }
+        
+        
+    })
+    
+})
 
 
